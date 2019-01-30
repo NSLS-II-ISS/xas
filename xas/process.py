29e1fc3be8b164a7f3c6263419ded195574135f0
elistavitski <elistavitski@gmail.com>
2019-01-30 16:21:35 -0500

    Added logging

--- xas/process.py
+++ xas/process.py
@@ -4,23 +4,33 @@ from .file_io import (load_dataset_from_files, create_file_header, validate_file
                       save_interpolated_df_as_file, save_binned_df_as_file, find_e0)
 from .interpolate import interpolate
 
+from .xas_logger import get_logger
+
 
 
 def process_interpolate_bin(doc, db, draw_func_interp = None, draw_func_bin = None):
+    logger = get_logger()
     if 'experiment' in db[doc['run_start']].start.keys():
         uid = doc['run_start']
         if db[uid].start['experiment'] == 'fly_energy_scan':
+            path_to_file = db[uid].start['interp_filename']
             raw_df = load_dataset_from_files(db, uid)
-            interpolated_df = interpolate(raw_df)
+            try:
+                interpolated_df = interpolate(raw_df)
+                logger.info(f'Interpolation successful for {path_to_file}')
+            except:
+                logger.info(f'Interpolation failed for {path_to_file}')
             e0 = find_e0(db,uid)
             comments = create_file_header(db,uid)
             validate_path_exists(db,uid)
-            path_to_file = db[uid].start['interp_filename']
+
             path_to_file = validate_file_exists(path_to_file, file_type = 'interp')
             print(f'>>>Path to file {path_to_file}')
             save_interpolated_df_as_file(path_to_file, interpolated_df, comments)
             if e0 >0:
+
                 binned_df = bin(interpolated_df,e0)
+
                 save_binned_df_as_file(path_to_file, binned_df, comments)
             else:
                 print('Energy E0 is not defined')
