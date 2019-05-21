6bc38581a7502a61f6d4a0807fcd242a58d4293e
elistavitski <elistavitski@gmail.com>
2019-05-21 16:59:18 -0400

    Added energy grid

--- xas/xray.py
+++ xas/xray.py
@@ -90,3 +90,31 @@ def energy2encoder(energy, pulses_per_deg, offset = 0):
     """
     return pulses_per_deg * (np.degrees(np.arcsin(-12400 / (2 * 3.1356 * energy))) - float(offset))
 
+
+
+def generate_energy_grid(e0, preedge_start, xanes_start, xanes_end, exafs_end, preedge_spacing,
+                        xanes_spacing, exafsk_spacing, int_time_preedge = 1, int_time_xanes = 1, int_time_exafs = 1, k_power = 0):
+    preedge = np.arange(e0 + preedge_start, e0 + xanes_start, preedge_spacing)
+    preedge_int = np.ones(len(preedge)) * int_time_preedge
+
+    edge = np.arange(e0 + xanes_start, e0 + xanes_end, xanes_spacing)
+    edge_int = np.ones(len(edge)) * int_time_xanes
+
+    iterator = exafsk_spacing
+    kenergy = 0
+    postedge = np.array([])
+
+    energy_end = k2e(exafs_end, e0)
+    exafs_int = []
+    while(kenergy + e0 + xanes_end < energy_end):
+        kenergy = k2e(iterator, e0) - e0
+
+        postedge = np.append(postedge, e0 + xanes_end + kenergy)
+        k_current = e2k(e0 + xanes_end + kenergy,e0)
+        exafs_int.append(int_time_exafs * (k_current ** k_power))
+        iterator += exafsk_spacing
+
+    integration_times = np.append(np.append(preedge_int, edge_int), np.array(exafs_int))
+    grid = np.append(np.append(preedge, edge), postedge)
+    return grid, integration_times
+    #return np.append(np.append(preedge, edge), postedge)
