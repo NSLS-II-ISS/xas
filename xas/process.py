673e7c101276652efa0c99e99472a9a67c72ef0b
dleshchev <leshchev.denis@gmail.com>
2021-10-22 17:26:15 -0400

    added xasproject conversion into a 2d dataset

--- xas/process.py
+++ xas/process.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from isscloudtools.slack import slack_upload_image
-from isstools.elements.cloud_plotting import generate_output_figures
+from isscloudtools.cloud_dispatcher import generate_output_figures
 from PIL import Image
 
 def process_interpolate_bin(doc, db, draw_func_interp = None, draw_func_bin = None, cloud_dispatcher = None):
