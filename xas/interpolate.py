97684db6bc9e03e6ae02b9244ea26ca10cf34e61
dleshchev leshchev.denis@gmail.com <leshchev.denis@gmail.com>
2022-06-28 13:00:11 -0400

    WIP interpolation

--- xas/interpolate.py
+++ xas/interpolate.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from scipy.interpolate import interp1d
 
 
 def interpolate(dataset, key_base = None, sort=True):
@@ -21,21 +21,32 @@ def interpolate(dataset, key_base = None, sort=True):
     timestamps = timestamps[np.sum(condition):]
 
     condition = timestamps > max_timestamp
-    timestamps = timestamps[: len(timestamps) - np.sum(condition)]
+    timestamps = timestamps[: (len(timestamps) - np.sum(condition) - 1)]
+
+    interpolated_dataset['timestamp'] = timestamps.values
 
     for key in dataset.keys():
         if len(dataset.get(key).iloc[:, 0]) > 5 * len(timestamps):
             time = [np.mean(array) for array in np.array_split(dataset.get(key).iloc[:, 0].values, len(timestamps))]
             val = [np.mean(array) for array in np.array_split(dataset.get(key).iloc[:, 1].values, len(timestamps))]
-            interpolated_dataset[key] = np.array([timestamps, np.interp(timestamps, time, val)]).transpose()
+            # interpolated_dataset[key] = np.array([timestamps, np.interp(timestamps, time, val)]).transpose()
         else:
-            interpolated_dataset[key] = np.array([timestamps, np.interp(timestamps, dataset.get(key).iloc[: ,0].values,
-                                                                        dataset.get(key).iloc[:,1])]).transpose()
-    intepolated_dataframe = pd.DataFrame(np.vstack((timestamps, np.array([interpolated_dataset[array][:, 1] for
-                                                                            array in interpolated_dataset]))).transpose())
-    keys = ['timestamp']
-    keys.extend(interpolated_dataset.keys())
-    intepolated_dataframe.columns = keys
+            time = dataset.get(key).iloc[:, 0].values
+            val = dataset.get(key).iloc[:, 1].values
+        # interpolated_dataset[key] = np.array([timestamps, np.interp(timestamps, time, val)]).transpose()
+        interpolator_func = interp1d(time, np.array([v for v in val]), axis=0)
+        val_interp = interpolator_func(timestamps)
+        if len(val_interp.shape) == 1:
+            interpolated_dataset[key] = val_interp
+        else:
+            interpolated_dataset[key] = [v for v in val_interp]
+
+    intepolated_dataframe = pd.DataFrame(interpolated_dataset)
+    # intepolated_dataframe = pd.DataFrame(np.vstack((timestamps, np.array([interpolated_dataset[key][:, 1] for
+    #                                                                         key in interpolated_dataset.keys()]))).transpose())
+    # keys = ['timestamp']
+    # keys.extend(interpolated_dataset.keys())
+    # intepolated_dataframe.columns = keys
 
     # intepolated_dataframe['mu_t'] = np.log( intepolated_dataframe['i0'] / intepolated_dataframe['it'] )
     # intepolated_dataframe['mu_f'] = intepolated_dataframe['iff'] / intepolated_dataframe['i0']
