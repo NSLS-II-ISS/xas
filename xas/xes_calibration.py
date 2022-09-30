01b771289bf2a74a7a7983836281d7115cc643af
Charles Clark <Charles.Clark@stonybrook.edu>
2022-09-30 12:36:31 -0400

    fit plane to data and 3d visualization

--- xas/xes_calibration.py
+++ xas/xes_calibration.py
@@ -1,12 +1,14 @@
 import json
 import matplotlib
+from matplotlib import projections
 matplotlib.use('TkAgg')  # something wrong with my (Charlie's) system Qt 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.ndimage import center_of_mass
-from scipy.interpolate import interp1d
-from scipy import stats
+from scipy.ndimage import center_of_mass, rotate
+from scipy import stats, linalg
+
+from mpl_toolkits.mplot3d import Axes3D
 
 PATH = '/home/charles/Desktop/XES_calib'
 DATA_FILE = 'Cu_calibration.json'
@@ -17,6 +19,19 @@ def percentile_threshold(im2d, p):
    """set values below percentile equal to 0"""
    im2d[im2d < np.percentile(im2d, p)] = 0
    return im2d
+
+
+def fit_plane(X, Y, Z):
+    """ fit plane to 3d data using least square regression"""
+    X, Y, Z = np.array(X), np.array(Y), np.array(Z)
+    assert(X.size == Y.size == Z.size)
+    A = np.column_stack([X, Y, np.ones(X.size)])
+    C, _, _, _ = linalg.lstsq(A, Z)
+    
+    # equation for plane: z = a*x + b*y + c
+    a, b, c = C[0], C[1], C[2]
+    return a, b, c
+
 
 def main(data_file=f'{PATH}/{DATA_FILE}', md_file=f'{PATH}/{MD_FILE}'):
     
@@ -42,7 +57,8 @@ def main(data_file=f'{PATH}/{DATA_FILE}', md_file=f'{PATH}/{MD_FILE}'):
     max_y = roi1['y'] + dy
 
     pix_roi = pix_array[:, min_y:max_y, min_x:max_x]
-    print(pix_roi[0])
+    for i, image in enumerate(pix_roi):
+        pix_roi[i] = rotate(image, 3, reshape=False)
 
     filtered_roi = np.zeros(pix_roi.shape)
 
@@ -59,24 +75,37 @@ def main(data_file=f'{PATH}/{DATA_FILE}', md_file=f'{PATH}/{MD_FILE}'):
     energies = data['hhm_energy']
     assert(pix_roi.shape[0] == len(energies))
 
-    slope, intercept, _, _, _ = stats.linregress(COM['x'], energies)
-    def energy_function(x):
-        return intercept + slope * x
+    # slope, intercept, _, _, _ = stats.linregress(COM['x'], energies)
+    a, b, c = fit_plane(COM['x'], COM['y'], energies)
+    def energy_function(x, y):
+        # return intercept + slope * x
+        return a*x + b*y + c
 
     # generate energy map with same dimensions as roi image
     energy_map = np.zeros(pix_roi[0].shape)
     for y, x in np.ndindex(energy_map.shape):
         # apply energy function to each point on map
-        energy_map[y, x] = energy_function(x)
+        energy_map[y, x] = energy_function(x, y)
 
     ax1 = plt.subplot(211)
     ax1.imshow(np.sum(pix_roi, axis=0))
     for x, y in zip(COM['x'], COM['y']):
         ax1.plot(x, y, 'o', c='r')
+    for x, energy in zip(COM['x'], energies):
+        plt.axvline(x, c='orange')
+        plt.text(x+1, 3, f'{energy}', c='orange')
 
     ax2 = plt.subplot(212)
     ax2.imshow(energy_map, cmap='OrRd')
 
+    plt.show()
+
+    # 3D plot
+    plt.clf()
+    ax = plt.axes(projection='3d')
+    ax.scatter(COM['x'], COM['y'], energies, c='darkblue')
+    x_grid, y_grid = np.meshgrid(np.arange(0, energy_map.shape[1]), np.arange(0, energy_map.shape[0]))
+    ax.plot_surface(x_grid, y_grid, energy_map, cmap='OrRd')
     plt.show()
 
 if __name__ == '__main__':
