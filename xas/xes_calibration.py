75bd3a27b31ed64e15b7eafbe2a9dadd0a1338bc
Charles Clark <Charles.Clark@stonybrook.edu>
2022-10-03 14:13:04 -0400

    project x,y onto best fit line

--- xas/xes_calibration.py
+++ xas/xes_calibration.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.ndimage import center_of_mass, rotate
-from scipy import stats, linalg
+from scipy import linalg
 
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -16,13 +16,13 @@ MD_FILE = 'Cu_calibration_md.json'
 
 
 def percentile_threshold(im2d, p):
-   """set values below percentile equal to 0"""
+   """ set values below percentile equal to 0 """
    im2d[im2d < np.percentile(im2d, p)] = 0
    return im2d
 
 
 def fit_plane(X, Y, Z):
-    """ fit plane to 3d data using least square regression"""
+    """ fit plane to 3d data using least square regression """
     X, Y, Z = np.array(X), np.array(Y), np.array(Z)
     assert(X.size == Y.size == Z.size)
     A = np.column_stack([X, Y, np.ones(X.size)])
@@ -34,11 +34,22 @@ def fit_plane(X, Y, Z):
 
 
 def file_io(data_file, md_file):
-    # Load data and metadata
+    """ load data and metadata from local files """
     data = pd.read_json(data_file)
     with open(md_file) as metadata:
         md = json.load(metadata)[1]
     return data, md
+
+
+def fit_xy(X, Y):
+    X,Y = np.array(X), np.array(Y)
+    slope, intercept = np.polyfit(X, Y, 1)
+    return slope, intercept
+
+def project_pt2line(x_pt, y_pt, slope, intercept):
+    x_proj = (x_pt + slope*y_pt - slope*intercept) / (slope**2 + 1)
+    y_proj = slope * x_proj + intercept
+    return x_proj, y_proj
 
 def run_calibration(data, md, roi='roi1'):
     
@@ -59,8 +70,8 @@ def run_calibration(data, md, roi='roi1'):
     max_y = roi_['y'] + dy
 
     pix_roi = pix_array[:, min_y:max_y, min_x:max_x]
-    # for i, image in enumerate(pix_roi):
-    #     pix_roi[i] = rotate(image, 3, reshape=False)
+    for i, image in enumerate(pix_roi):
+        pix_roi[i] = rotate(image, 3, reshape=False)
 
     filtered_roi = np.zeros(pix_roi.shape)
 
@@ -77,11 +88,13 @@ def run_calibration(data, md, roi='roi1'):
     energies = data['hhm_energy']
     assert(pix_roi.shape[0] == len(energies))
 
-    slope, intercept, _, _, _ = stats.linregress(COM['x'], energies)
+    slope_xy, int_xy = fit_xy(COM['x'], COM['y'])
+    energy_fit = np.poly1d(np.polyfit(COM['x'], energies, 2))
     # a, b, c = fit_plane(COM['x'], COM['y'], energies)
     def energy_function(x, y):
-        return intercept + slope * x
-        # return a*x + b*y + c
+        x_p, y_p = project_pt2line(x, y, slope_xy, int_xy)
+        energy = energy_fit(x_p)
+        return energy
 
     # generate energy map with same dimensions as roi image
     energy_map = np.zeros(pix_roi[0].shape)
@@ -97,22 +110,25 @@ def run_calibration(data, md, roi='roi1'):
         plt.axvline(x, c='orange')
         plt.text(x+1, 3, f'{energy}', c='orange')
 
+    x_grid, y_grid = np.meshgrid(np.arange(0, energy_map.shape[1]), np.arange(0, energy_map.shape[0]))
     ax2 = plt.subplot(212)
-    ax2.imshow(energy_map, cmap='OrRd')
+    ax2.contour(energy_map, np.flip(energies), cmap='Reds')
+    ax2.imshow(energy_map, cmap='gray')
+    _x = np.arange(0, energy_map.shape[1], 1)
+    _y = slope_xy * _x + int_xy
 
+    ax2.plot(_x, _y, '-', c='r')
     plt.show()
+
+    # print(energy_map[:,100])
 
     # 3D plot
     plt.clf()
     ax = plt.axes(projection='3d')
     ax.scatter(COM['x'], COM['y'], energies, c='darkblue')
-    x_grid, y_grid = np.meshgrid(np.arange(0, energy_map.shape[1]), np.arange(0, energy_map.shape[0]))
-    ax.plot_surface(x_grid, y_grid, energy_map, cmap='OrRd', alpha=0.5)
+    ax.plot_surface(x_grid, y_grid, energy_map, cmap='viridis', alpha=0.5)
     plt.show()
 
-    plt.clf()
-    plt.plot(COM['x'], energies, 'o-')
-    plt.show()
 
 
 
