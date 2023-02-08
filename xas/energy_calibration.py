8c72b44a212498f6db459d57d2405b97d581b6f6
Charles Clark <Charles.Clark@stonybrook.edu>
2023-02-08 16:13:03 -0500

    compute energy offset and broadening from spyder

--- xas/energy_calibration.py
+++ xas/energy_calibration.py
@@ -39,6 +39,31 @@ def compute_shift_between_spectra(energy, mu, energy_ref_roi, mu_ref_roi):
     mu_fit = interpolated_spectrum(out.params)
     return e_shift, mu_fit
 
+def compute_shift_between_spectra_alt(energy, mu, energy_ref, mu_ref, e0=8333, de=50):
+    roi_mask = (energy_ref > (e0 - de / 2)) & (energy_ref < (e0 + de / 2))
+    energy_ref_roi = energy_ref[roi_mask]
+    mu_ref_roi = mu_ref[roi_mask]
+    
+    energy_ref_roi_norm = (energy_ref_roi - energy_ref_roi.min()) / (energy_ref_roi.max() - energy_ref_roi.min())
+    
+    def interpolated_spectrum(pars):
+        e_shift = pars.valuesdict()['e_shift']
+        x = np.interp(energy_ref_roi, energy - e_shift, mu)
+        basis = np.vstack((np.ones(x.shape), x, energy_ref_roi_norm, energy_ref_roi_norm**2, energy_ref_roi_norm**3)).T
+        c, _, _, _ = np.linalg.lstsq(basis, mu_ref_roi, rcond=-1)
+        return basis @ c
+
+    def residuals(pars):
+        return (interpolated_spectrum(pars) - mu_ref_roi)
+
+    pars = Parameters()
+    pars.add('e_shift', value=0)
+    out = minimize(residuals, pars)
+    e_shift = out.params['e_shift'].value
+    mu_fit = interpolated_spectrum(out.params)
+    return e_shift, energy_ref_roi, mu_fit
+
+
 def gaussian_conv_matrix(t_in, t_out, sigma):
     """Gaussian convolution matrix. Normalized by row sum."""
     # sigma = fwhm / 2.355
@@ -53,11 +78,45 @@ def conv_spectrum(energy_in, energy_out, mu_in, sigma):
     return conv_matrix @ mu_in
 
 
-def roi_crop_spectrum(energy, mu, e0, de):
+def compute_energy_offset_and_broadening(energy, mu, energy_ref, mu_ref, e0=8333, de=200):
+    
+    cs = CubicSpline(energy_ref, mu_ref)
+    
     roi_mask = (energy > (e0 - de / 2)) & (energy < (e0 + de / 2))
     energy_roi = energy[roi_mask]
     mu_roi = mu[roi_mask]
-    return energy_roi, mu_roi
+    
+    energy_roi_norm = (energy_roi - energy_roi.min()) / (energy_roi.max() - energy_roi.min())
+    
+    def get_mu_fit(pars):
+        shift = pars.valuesdict()['shift']
+        sigma = pars.valuesdict()['sigma']
+        fine_grid_energy_ref = np.arange(energy_ref.min(), energy_ref.max(), sigma/10)
+        fine_grid_mu_ref = cs(fine_grid_energy_ref)
+        # print(sigma)
+        mu_ref_conv = conv_spectrum(
+            fine_grid_energy_ref - shift, energy_roi, fine_grid_mu_ref, sigma=sigma
+            )
+        
+        basis = np.vstack(
+            (mu_ref_conv, np.ones(energy_roi.shape), energy_roi_norm)
+            ).T
+        c, _, _, _ = np.linalg.lstsq(basis, mu_roi, rcond=-1)
+        return basis @ c
+    
+    def residuals(pars):
+        return get_mu_fit(pars) - mu_roi
+    
+    shift_guess = compute_shift_between_spectra_alt(energy_ref, mu_ref, energy, mu, e0=e0, de=de)[0]
+    print(shift_guess)
+    pars = Parameters()
+    pars.add("sigma", value=0.01, min=0)
+    pars.add("shift", value=shift_guess)
+    out = minimize(residuals, pars)
+    sigma = out.params["sigma"].value
+    shift = out.params["shift"].value
+    print(fit_report(out))
+    return shift, sigma, energy_roi, get_mu_fit(pars)
 
 
 def compute_energy_offset_and_broadening(energy_roi, mu_roi, energy_ref, mu_ref):
@@ -84,7 +143,7 @@ def compute_energy_offset_and_broadening(energy_roi, mu_roi, energy_ref, mu_ref)
     def residuals(pars):
         return get_mu_fit(pars) - mu_roi
     
-    shift_guess = compute_shift_between_spectra(energy_ref, mu_ref, energy, mu_roi)
+    shift_guess = compute_shift_between_spectra_alt(energy_ref, mu_ref, energy, mu_roi)
     pars = Parameters()
     pars.add("sigma", value=0.01, min=0)
     pars.add("shift", value=shift_guess)
