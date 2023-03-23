1dc844c44f638e5ea0469145ee02537011fc4501
Denis Leshchev <leshchev.denis@gmail.com>
2023-03-23 17:18:57 -0400

    drafting of cached tiled entry class

--- xas/tiled_io.py
+++ xas/tiled_io.py
@@ -5,18 +5,56 @@ from tiled.queries import Key
 from collections import UserDict
 
 
-class StoredTiledEntry:
-    def __init__(self, client_obj):
-        self.source_type = type(client_obj)
-        self.data = client_obj.read()
-        self.metadata = client_obj.metadata
+def PROCESS_MY_DATASET(data):
+    data_type = 'xas'
 
-    def read(self):
-        """implemented to closer mimic tiled client object funcionality"""
-        return self.data
+    if data_type == 'xas':
+        default_kwargs = {.....}
+        processed_data = larch_normalize_this_somehow(data, **default_kwargs) # ideally at some point AIML people will take care of kwargs and whatnot
+
+
+    return processed_data,
+
+def function_for_xas_processing():
+    pass
+
+
+mapping_of_processing = {'xas' : function_for_xas_processing}
+
+
+
+
+class StoredTiledEntry(UserDict):
+    def __init__(self, client_obj):
+        super().__init__()
+        self.client_obj = client_obj
+        # self.source_type = type(client_obj)
+        # self.data = client_obj.read()
+        # self.metadata = client_obj.metadata
+
+    # def read(self):
+    #     """implemented to closer mimic tiled client object funcionality"""
+    #     return self.data
     
     def __repr__(self):
         return f"Stored data and metadata from {self.source_type}"
+
+    def __getitem__(self, kind):
+        if kind in self.data.keys():
+            return self.data[kind]
+        else:
+            if kind == 'binned':
+                data = self.client_obj.read()
+                metadata = self.client_obj.metadata
+
+            elif kind == 'processed':
+                data, metadata = PROCESS_MY_DATASET(self.data['binned'])
+                self.data[kind] = {'data': data, 'metadata': metadata}
+            else:
+                raise KeyError('NOT IMPLEMENTED')
+                # see if you have it
+
+    # def process(self):
 
 
 class TiledReader(UserDict):
@@ -25,7 +63,8 @@ class TiledReader(UserDict):
     attribute and pulled from there in the future."""
 
     def __init__(self, source_node: Node):
-        self.data = {}
+        super().__init__()
+        # self.data = {}
         self.source_node = source_node
 
     def __setitem__(self, *args):
